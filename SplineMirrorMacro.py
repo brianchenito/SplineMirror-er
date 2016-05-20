860329e227a67c8fc430ad4d7bd39a716ce7b31c
Brian Chen <brianchenito@gmail.com>
2016-05-20 15:02:20 -0700

    Basic functionality achieved
    -renaming system
    -auto recoloring
    -mirroring on RH coord system

--- SplineMirrorMacro.py
+++ SplineMirrorMacro.py
@@ -1,11 +1,23 @@
+'''
+A script for mirroring group/spline trees in maya. 
+for right handed coordinates only, mirrors over the YZ plane.
+allows for the renaming of all objects in tree, and recolors 
+splines red or blue if using right/left naming conventions. 
+'''
+
 import pymel.core as pm
 
 for item in pm.selected():
 	if pm.objectType(item, isType='transform'):
+		trans1=pm.getAttr(item.translateX)*(-1) 
+		trans2=pm.getAttr(item.scaleX)*(-1)
+		trans3=pm.getAttr(item.rotateY)*(-1)
 		new=pm.duplicate(item)
 		oldname='null'
 		replacename='null'
 		docolor=False
+		color=0
+		oldcolor=0
 		temp=pm.promptDialog(
 			message='Find in: \n'+item.name(),
 			button=['Ok','Cancel'],
@@ -32,28 +44,27 @@ for item in pm.selected():
 					query=True,
 					text=True
 					)
-				if 'l' in oldname.lower() and 'r' in replacename.lower:
-					docolor=True
-					color=13
-				else if 'r' in oldname.lower() and 'l' in replacename.lower:
+				if oldname.lower().find('l') and replacename.lower().find('r'):
 					docolor=True
 					color=6
+					oldcolor=13
+				elif oldname.lower().find('r') and replacename.lower().find('l'):
+					docolor=True
+					color=13
+					oldcolor=6
 
 
 				for i in new:
-					i.rename(i.name().replace (str(old+'1'),str(replace)))
-					for j in pm.listRelatives(i, allDescendents=True, type='transform'):
-						j.rename(j.name().replace (str(old),str(replace)))
-					
-					
-				i=pm.getAttr(item.translateX)*(-1)
-				j=pm.getAttr(item.scaleX)*(-1)
-				k=pm.getAttr(item.rotateY)*(-1)
-				for duped in new:
-					pm.setAttr(duped.translateX, i)
-					pm.setAttr(duped.scaleX,j)
-					pm.setAttr(duped.rotateY,k)
-					pm.settattr(duped.overrideEnabled,1)
-					pm.settattr(duped.overrideColor,color)
+					i.rename(i.name().replace (str(oldname+'1'),str(replacename)))
+					pm.setAttr(i.translateX, trans1)
+					pm.setAttr(i.scaleX,trans2)
+					pm.setAttr(i.rotateY,trans3)
 
-			
+					for j in pm.listRelatives(i, allDescendents=True, type='transform'):
+						j.rename(j.name().replace (str(oldname),str(replacename)))
+						if docolor and j.overrideColor.get()==oldcolor:
+							j.overrideEnabled.set(True)
+							j.overrideColor.set(color)
+
+					
+
